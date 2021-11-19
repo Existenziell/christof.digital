aff22ff4c6b517e3f3bf21bb22b3ec4bb51b2afc
Christof <bauer.christof@gmail.com>
2021-11-18 22:03:59 -0500

    Make inputs required on /contact

--- pages/contact.js
+++ pages/contact.js
@@ -37,9 +37,9 @@ const Contact = () => {
 
             <div className="p-12 mt-6 shadow w-full lg:w-1/2 bg-white">
                 <form onSubmit={submitForm} className="flex flex-col space-y-6 ">
-                    <input type="text" name="name" placeholder="Your Name" onChange={setData}></input>
-                    <input type="email" name="email" placeholder="Your Email" onChange={setData}></input>
-                    <textarea placeholder="Your Message" name="message" onChange={setData} rows="10"></textarea>
+                    <input type="text" name="name" placeholder="Your Name" onChange={setData} required></input>
+                    <input type="email" name="email" placeholder="Your Email" onChange={setData} required></input>
+                    <textarea placeholder="Your Message" name="message" onChange={setData} rows="10" required></textarea>
 
                     {errorMsg ?
                         <div className='text-left bg-red-700 text-white p-4'>
