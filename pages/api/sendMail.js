02e9de2de1934fb7bda795bacde751cc8beb1bce
Christof <bauer.christof@gmail.com>
2021-11-18 22:47:18 -0500

    Update sendMail.js

--- pages/api/sendMail.js
+++ pages/api/sendMail.js
@@ -2,20 +2,22 @@ import sgMail from '@sendgrid/mail'
 
 sgMail.setApiKey(process.env.NEXT_PUBLIC_MAIL_API_KEY);
 
-export default async (req, res) => {
+const sendMail = async (req, res) => {
   const { name, email, message } = req.body
   const msg = {
     to: 'bauer.christof@gmail.com',
     from: 'love@christof.digital',
-    subject: `Contact request from LOVE`,
-    text: "Hello"
-    // html: message
+    subject: `Contact request from ${name} - ${email}`,
+    text: message,
+    html: message
   }
 
   try {
     await sgMail.send(msg)
     res.status(200).json({ message: `Email has been sent` })
   } catch (error) {
-    res.status(500).json({ error: `Error sending email ${error.message}` })
+    res.status(500).json({ error })
   }
 }
+
+export default sendMail
