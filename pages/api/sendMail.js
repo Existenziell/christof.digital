fa394e2b54ea85b6dc7a5745f7168a04a9a2648a
Christof <bauer.christof@gmail.com>
2021-11-18 20:23:17 -0500

    Assign arrow function to a variable before exporting as module default  import/no-anonymous-default-export

--- pages/api/sendMail.js
+++ pages/api/sendMail.js
@@ -2,7 +2,7 @@ import sgMail from '@sendgrid/mail'
 
 sgMail.setApiKey(process.env.NEXT_MAIL_API_KEY);
 
-export default async (req, res) => {
+const sendMail = async (req, res) => {
   const { name, email, message } = req.body
   const msg = {
     to: 'bauer.christof@gmail.com',
@@ -16,6 +16,8 @@ export default async (req, res) => {
     await sgMail.send(msg)
     res.status(200).json({ message: `Email has been sent` })
   } catch (error) {
-    res.status(500).json({ error: 'Error sending email' })
+    res.status(500).json({ error })
   }
 }
+
+export default sendMail
