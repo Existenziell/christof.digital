b41b75c911761dba91cdc1946461f406e7385d97
Christof <bauer.christof@gmail.com>
2021-11-19 13:05:12 -0500

    Update coding.js

--- pages/coding.js
+++ pages/coding.js
@@ -14,7 +14,7 @@ const Coding = () => {
 
   return (
     <div className="items-center justify-content text-left w-full">
-      <h1 className="text-2xl mb-8">I love Code!</h1>
+      <h1 className="text-4xl mb-8 text-center">I love Code!</h1>
 
       <SyntaxHighlighter language="javascript" style={darcula} showLineNumbers={true}>
         {code}
@@ -23,7 +23,7 @@ const Coding = () => {
       <p className='mt-8 md:w-1/2'>
         I have been developing Code since the year 2000.
         Web-Development especially has been changing so much over the years, it has been and still is a pleasure to witness all of this.
-        Having worked mostly in backend for years (PHP, Ruby on Rails, Java, nodeJS, ...) I later went Fullstack into JavaScript and nowadays this distinction is rarely made any more.
+        Having worked mostly in the backend for years (PHP, Ruby on Rails, Java, nodeJS, ...) I later went Fullstack into JavaScript and nowadays this distinction is rarely made any more.
         Modern tooling and build chains are just amazing and make me enjoy development even more today.
       </p>
       <div className='mt-8'>
