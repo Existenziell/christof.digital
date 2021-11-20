55db7e266249fb3212a443063ed9b299193125fd
Christof <bauer.christof@gmail.com>
2021-11-19 19:55:40 -0500

    Update coding.js

--- pages/coding.js
+++ pages/coding.js
@@ -23,8 +23,13 @@ const Coding = () => {
       <p className='mt-8 md:w-1/2'>
         I have been developing Code since the year 2000.
         Web-Development especially has been changing so much over the years, it has been and still is a pleasure to witness all of this.
-        Having worked mostly in the backend for years (PHP, Ruby on Rails, Java, nodeJS, ...) I later went Fullstack into JavaScript and nowadays this distinction is rarely made any more.
-        Modern tooling and build chains are just amazing and make me enjoy development even more today.
+        Having worked mostly in the backend for years (PHP, Ruby on Rails, Java, nodeJS, ...) I later went Fullstack JavaScript and nowadays this distinction is rarely made any more.
+        Modern tooling and build chains are just amazing and make me enjoy development even more today.<br />
+        Current stack: NextJS, TailwindCSS, Vercel, JAM Stack mostly.
+      </p>
+      <p className='mt-8 md:w-1/2'>
+        Recently I also ventured into the realms of the Blockchain, what a time to be alive! Web3 is just amazing and the pace of evolution absolutely breathtaking.
+        Current stack: Solidity, HardHat, ethers.js for smart contracts – NextJS, TailwindCSS, Vercel for the dApp.
       </p>
       <div className='mt-8'>
         <h2 className='text-xl mb-2'>Further resources:</h2>
