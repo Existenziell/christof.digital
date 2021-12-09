2ae7053f91df14b2f1b9c8ece7af6d66db9f5eba
Christof <bauer.christof@gmail.com>
2021-12-08 22:58:38 -0500

    New layout for /index on big screens

--- pages/index.js
+++ pages/index.js
@@ -12,20 +12,23 @@ export default function Index() {
         </a>
       </Link>
 
-      <div className='dark:bg-gray-300 rounded p-6'>
-        <Image src='/icons/logo.png' width={600} height={329} alt='Logo' />
-      </div>
-      <div className='mt-16 flex flex-col gap-8 leading-relaxed'>
-        <h1 className='text-4xl'>Hi, I am Christof</h1>
-        <p>You have come to the right place.<br />
-          I am a Fullstack App Developer (Web, Blockchain), travelling the world and looking for enticing projects and humans.<br />
-          Please <Link href='/contact'><a className='link'>contact me</a></Link> if you see possible synergies or have questions.
-        </p>
-        <p>
-          If you came here for Yoga, you&apos;ll find all information on{' '}
-          <Link href='http://yoga.christof.digital/'><a className='link' rel='noopener noreferrer nofollow'>yoga.christof.digital</a></Link>.
-        </p>
-      </div>
+      <section className='lg:flex lg:flex-row-reverse lg:items-center lg:justify-center lg:gap-4 lg:pb-32 w-full'>
+        <div className='dark:bg-gray-300 rounded p-6 lg:w-1/2 '>
+          <Image src='/icons/logo.png' width={600} height={329} alt='Logo' />
+        </div>
+        <div className='mt-12 lg:mt-0 flex flex-col gap-8 leading-relaxed lg:w-1/2 lg:text-right'>
+          <h1 className='text-4xl'>Hi, I am Christof</h1>
+          <p>You have come to the right place.<br />
+            I am a Fullstack App Developer (Web, Blockchain), travelling the world and looking for enticing projects and humans.
+            Please <Link href='/contact'><a className='link'>contact me</a></Link> if you see possible synergies or have questions.
+          </p>
+          <p>
+            If you came here for Yoga, you&apos;ll find all information on{' '}
+            <Link href='http://yoga.christof.digital/'><a className='link' rel='noopener noreferrer nofollow'>yoga.christof.digital</a></Link>.
+          </p>
+        </div>
+      </section>
+
     </div>
   )
 }
