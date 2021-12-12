40c9474da78a15b9ce81f92e41ec571d0bba7d5f
Christof <bauer.christof@gmail.com>
2021-12-12 15:05:22 -0500

    Update coding.js

--- pages/coding.js
+++ pages/coding.js
@@ -18,7 +18,7 @@ const Coding = () => {
       </Link>
 
       <h1 className='text-4xl md:text-6xl mb-8 text-center'>I love Code!</h1>
-      <div className='lg:w-2/3 lg:mx-auto'>
+      <div>
 
         <div className='hidden dark:block'>
           <SyntaxHighlighter language='javascript' style={dracula} showLineNumbers={true}>
@@ -31,21 +31,23 @@ const Coding = () => {
           </SyntaxHighlighter>
         </div>
 
-        <p className='mt-8'>
-          I have been developing Code since the year 2000.
-          Web-Development especially has been changing so much over the years, it has been and still is a pleasure to witness all of this.
-          Having worked mostly in the backend for years (PHP, Ruby on Rails, Java, nodeJS, ...) I later went Fullstack JavaScript and nowadays this distinction is rarely made any more.
-          Modern tooling and build chains are just amazing and make me enjoy development even more today.
-          <span className='text-sm block mt-2'>Current stack: NextJS, TailwindCSS, Vercel, JAM Stack mostly.</span>
-        </p>
-        <p className='mt-8'>
-          Recently I also ventured into the realms of the Blockchain, what a time to be alive! Web3 is just amazing and the pace of evolution absolutely breathtaking. The possibilities...!
-        </p>
-        <div className='mt-8'>
-          <h2 className='text-xl mb-2'>Further resources:</h2>
-          <Link href='/cv'><a className='link'>Curriculum Vitae</a></Link><br />
-          <Link href='/contact'><a className='link'>Contact me</a></Link><br />
-          <Link href='https://github.com/Existenziell'><a className='link' target='_blank' rel='noopener noreferrer nofollow'>Github</a></Link>
+        <div className='lg:w-2/3 lg:mx-auto'>
+          <p className='mt-8'>
+            I have been developing Code since the year 2000.
+            Web-Development especially has been changing so much over the years, it has been and still is a pleasure to witness all of this.
+            Having worked mostly in the backend for years (PHP, Ruby on Rails, Java, nodeJS, ...) I later went Fullstack JavaScript and nowadays this distinction is rarely made any more.
+            Modern tooling and build chains are just amazing and make me enjoy development even more today.
+            <span className='text-sm block mt-2'>Current stack: NextJS, TailwindCSS, Vercel, JAM Stack mostly.</span>
+          </p>
+          <p className='mt-8'>
+            Recently I also ventured into the realms of the Blockchain, what a time to be alive! Web3 is just amazing and the pace of evolution absolutely breathtaking. The possibilities...!
+          </p>
+          <div className='mt-8'>
+            <h2 className='text-xl mb-2'>Further resources:</h2>
+            <Link href='/cv'><a className='link'>Curriculum Vitae</a></Link><br />
+            <Link href='/contact'><a className='link'>Contact me</a></Link><br />
+            <Link href='https://github.com/Existenziell'><a className='link' target='_blank' rel='noopener noreferrer nofollow'>Github</a></Link>
+          </div>
         </div>
       </div>
 
