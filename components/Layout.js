5af07b9c6872f9673a50ca4eee71d2bd2caac465
Christof <bauer.christof@gmail.com>
2021-12-13 12:01:34 -0500

    [Debug] Update tailwind to 3.0.2

--- components/Layout.js
+++ components/Layout.js
@@ -15,8 +15,8 @@ export default function Layout({ children, title }) {
         <link rel='apple-touch-icon' sizes='180x180' href='/favicon/apple-touch-icon.png' />
         <link rel='icon' type='image/png' sizes='32x32' href='/favicon/favicon-32x32.png' />
         <link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Gotu&display=swap" rel="stylesheet" />
+        {/* <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Gotu&display=swap" rel="stylesheet" /> */}
       </Head>
       <div className='dark:bg-black h-full'>
         <Nav />
