8cf7a077022f2139cd7bbfc25546219b88dd85da
Christof <bauer.christof@gmail.com>
2022-10-08 16:46:16 -0500

    Lint

--- pages/ai.js
+++ pages/ai.js
@@ -107,7 +107,7 @@ const AI = () => {
 
       <p className='flex flex-col md:flex-row-reverse items-center justify-center text-center mx-auto w-full md:w-2/3 mb-16'>
         There have recently been an number of AI projects especially in the space of image generation that have absolutely amazed me.
-        Here are a few outcomes of my interactions with these new systems. Remember, that all these images have been created by Artificial Intelligence, the 'only' input is the prompt.
+        Here are a few outcomes of my interactions with these new systems. Remember, that all these images have been created by Artificial Intelligence, the &apos;only&apos; input is the prompt.
       </p>
       {creations.map(creation =>
         <div key={creation.prompt} className='flex flex-col md:flex-row items-center justify-center gap-4 md:gap-8 mb-16'>
