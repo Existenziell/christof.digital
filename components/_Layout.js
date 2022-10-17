3ae130fe44c17afe484339ec8a00e08e40ab8290
Christof <bauer.christof@gmail.com>
2022-10-16 23:54:52 -0500

    Move head to _app

--- components/_Layout.js
+++ components/_Layout.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import Footer from './Footer'
 import Nav from './Nav'
 import DarkModeToggle from './DarkModeToggle'
@@ -7,18 +6,6 @@ import NextNprogress from 'nextjs-progressbar'
 export default function Layout({ children }) {
   return (
     <>
-      <Head>
-        <title>christof.digital</title>
-        <meta name='description' content='Join me on a journey through my life | christof.digital | shift-happens' />
-        <link rel='icon' href='/favicon/favicon.ico' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/favicon/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicon/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="msapplication-TileColor" content="#242424" />
-        <meta name="theme-color" content="#242424" />
-      </Head>
-
       <NextNprogress
         color='var(--color-cta)'
         startPosition={0.3}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 import Layout from '../components/_Layout'
+import Head from 'next/head'
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 
@@ -7,6 +8,17 @@ function Zooloo({ Component, pageProps }) {
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>christof.digital</title>
+        <meta name='description' content='Join me on a journey through my life | christof.digital | shift-happens' />
+        <link rel='icon' href='/favicon/favicon.ico' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/favicon/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#242424" />
+        <meta name="theme-color" content="#242424" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
