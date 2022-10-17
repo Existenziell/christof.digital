84e1e8ca826a33079cd7754335a2072976bf852a
Christof <bauer.christof@gmail.com>
2022-10-17 00:14:35 -0500

    Fonts?

--- components/_Layout.js
+++ components/_Layout.js
@@ -1,17 +1,17 @@
-import Footer from './Footer'
-import Nav from './Nav'
-import DarkModeToggle from './DarkModeToggle'
 import NextNprogress from 'nextjs-progressbar'
+import DarkModeToggle from './DarkModeToggle'
+import Nav from './Nav'
+import Footer from './Footer'
 
 export default function Layout({ children }) {
   return (
     <>
       <NextNprogress
-        color='var(--color-cta)'
         startPosition={0.3}
         stopDelayMs={100}
         height={3}
         showOnShallow={true}
+        color='var(--color-cta)'
       />
 
       <div className='flex justify-between items-start h-full p-4 md:px-8 bg-brand text-brand-dark dark:bg-brand-dark dark:text-brand'>
