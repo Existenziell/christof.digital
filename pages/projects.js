8add610f15fbe60a56aa7962353d418fcebd450d
Christof <bauer.christof@gmail.com>
2022-10-20 14:34:07 -0500

    New Layout for projects

--- pages/projects.js
+++ pages/projects.js
@@ -33,16 +33,9 @@ const Projects = () => {
         <div className='w-full text-left flex flex-wrap justify-evenly items-start mt-6 gap-4'>
           {data.map(project => (
             <div key={project.name}
-              className='w-full md:max-w-[calc(40vw)] lg:max-w-[calc(30vw)] p-4 mb-8 hover:scale-105 hover:shadow-2xl transition-all duration-500 bg-gray dark:bg-gray-dark text-brand-dark dark:text-brand rounded-sm'>
+              className='flex flex-col md:flex-row gap-8 w-full p-4 mb-8 bg-gray dark:bg-gray-dark text-brand-dark dark:text-brand rounded'>
 
-              <div className='flex justify-between w-full items-center mb-4'>
-                <h2 className='text-2xl truncate' title={project.name}>{project.name}</h2>
-                <ProjectLink href={project.link} external={project.external}>
-                  <LinkIcon className='w-6 hover:text-cta' />
-                </ProjectLink>
-              </div>
-              <p className='text-sm mb-2 h-16'>{project.desc}</p>
-              <div className='nextimg mb-4'>
+              <div className='nextimg md:mb-4 hover:scale-105 hover:shadow-2xl transition-all duration-500'>
                 <ProjectLink href={project.link} classes={'w-6 hover:text-cta'} external={project.external}>
                   <Image
                     src={`/projects/${project.image}`}
@@ -51,11 +44,24 @@ const Projects = () => {
                     height={600}
                     placeholder="blur"
                     blurDataURL={`/projects/${project.image}`}
+                    className=''
                   />
                 </ProjectLink>
               </div>
+              <div className='flex flex-col justify-between w-full items-start md:mb-4'>
 
-              <TagList items={project.tech} />
+                <div className='mb-8 md:mb-0'>
+                  <div className='flex justify-between items-center w-full'>
+                    <h2 className='text-2xl truncate' title={project.name}>{project.name}</h2>
+                    <ProjectLink href={project.link} external={project.external}>
+                      <LinkIcon className='w-6 hover:text-cta' />
+                    </ProjectLink>
+                  </div>
+                  <p className='text-sm mt-2 md:mt-8'>{project.desc}</p>
+                </div>
+                <TagList items={project.tech} />
+
+              </div>
             </div>
           ))}
         </div>
