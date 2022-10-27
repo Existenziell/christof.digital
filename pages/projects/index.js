c4445173b624c085a4473f2b8cde2d2b7620aa9f
Christof <bauer.christof@gmail.com>
2022-10-27 17:06:19 -0500

    Projects Grid again

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -26,16 +26,23 @@ const Projects = () => {
 
       <div className='w-full'>
         <h1 className='text-4xl md:text-6xl mb-4 text-center'>Projects</h1>
-        <p className='mx-auto max-w-xl text-center mb-16'>I thoroughly enjoy working on these smaller projects, as a tech demo, a MVP or just to play around with a new technology. Here are a few examples:</p>
+        <p className='mx-auto max-w-xl text-center mb-12'>I thoroughly enjoy working on these smaller projects, as a tech demo, a MVP or just to play around with a new technology. Here are a few examples:</p>
         <div className='flex items-center justify-center w-full'>
           <Sorting sortBy={sortBy} toggleSortBy={toggleSortBy} />
         </div>
-        <div className='w-full text-left flex flex-wrap justify-evenly items-start mt-6 gap-4'>
+        <div className='w-full text-left grid md:grid-cols-2 lg:grid-cols-3 mt-6 gap-8'>
           {data.map(project => (
             <div key={project.name}
-              className='flex flex-col md:flex-row gap-8 w-full p-4 mb-8 bg-gray dark:bg-gray-dark text-brand-dark dark:text-brand rounded'>
+              className='p-4 hover:scale-[102%] hover:shadow-2xl duration-500 bg-gray dark:bg-gray-dark text-brand-dark dark:text-brand rounded shadow transition-all'>
 
-              <div className='nextimg md:mb-4 hover:scale-105 hover:shadow-2xl transition-all duration-500'>
+              <div className='flex justify-between items-center gap-2 w-full mb-4'>
+                <h2 className='text-2xl truncate' title={project.name}>{project.name}</h2>
+                <ProjectLink href={project.link} external={project.external}>
+                  <LinkIcon className='w-6 hover:text-cta' />
+                </ProjectLink>
+              </div>
+
+              <div className='nextimg mb-6'>
                 <ProjectLink href={project.link} classes={'w-6 hover:text-cta'} external={project.external}>
                   <Image
                     src={`/projects/${project.image}`}
@@ -44,24 +51,13 @@ const Projects = () => {
                     height={600}
                     placeholder="blur"
                     blurDataURL={`/projects/${project.image}`}
-                    className=''
+                    className='rounded'
                   />
                 </ProjectLink>
               </div>
-              <div className='flex flex-col justify-between w-full items-start md:mb-4'>
 
-                <div className='mb-8 md:mb-0 w-full'>
-                  <div className='flex justify-between items-center w-full'>
-                    <h2 className='text-2xl truncate' title={project.name}>{project.name}</h2>
-                    <ProjectLink href={project.link} external={project.external}>
-                      <LinkIcon className='w-6 hover:text-cta' />
-                    </ProjectLink>
-                  </div>
-                  <p className='text-sm mt-2 md:mt-8'>{project.desc}</p>
-                </div>
-                <TagList items={project.tech} />
-
-              </div>
+              <p className='text-sm mb-2'>{project.desc}</p>
+              <TagList items={project.tech} />
             </div>
           ))}
         </div>
