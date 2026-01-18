cce8295160b58e991611181cd241b52aa802f817
Christof <bauer.christof@gmail.com>
2026-01-18 14:32:19 +0100

    feat: update links

--- components/BitcoinLauncher.js
+++ components/BitcoinLauncher.js
@@ -1,7 +1,7 @@
 const BitcoinLauncher = () => {
   return (
     <a
-      href='http://bitcoin.christof.digital'
+      href='http://bplusplus.info'
       target='_blank'
       rel='noopener noreferrer nofollow'
       className='fixed right-2 sm:right-0 bottom-28 block py-2 px-1 sm:p-2 text-sm transition-all bg-cta text-gray-dark hover:px-3 h-20'
