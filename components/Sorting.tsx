23af48636cabded07e552493ecab23ba1ddb2140
Christof <bauer.christof@gmail.com>
2026-02-22 15:49:48 +0100

    Update Sorting.tsx

--- components/Sorting.tsx
+++ components/Sorting.tsx
@@ -1,21 +1,25 @@
-import { Switch } from '@headlessui/react'
+import { Field, Label, Switch } from '@headlessui/react'
 import type { SortingProps } from '@/types'
 
 export default function Sorting({ sortBy, toggleSortBy }: SortingProps) {
   return (
-    <Switch.Group>
+    <Field>
       <div className="flex items-center text-sm">
-        <Switch.Label className="mr-3">Newer</Switch.Label>
+        <Label passive className="mr-3">
+          Newer
+        </Label>
         <Switch
-          aria-label='Switch Sorting'
+          aria-label="Sort by date: Newer or Older"
           checked={sortBy}
           onChange={toggleSortBy}
           className="bg-surface border border-border relative inline-flex items-center h-6 rounded-sm w-11 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-surface ring-primary"
         >
           <span className={`${sortBy ? 'translate-x-6' : 'translate-x-1'} inline-block w-4 h-4 transform bg-cta rounded-sm transition-transform`} />
         </Switch>
-        <Switch.Label className="ml-3">Older</Switch.Label>
+        <Label passive className="ml-3">
+          Older
+        </Label>
       </div>
-    </Switch.Group>
+    </Field>
   )
 }
